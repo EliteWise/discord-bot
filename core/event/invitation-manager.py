87c7ec1287938220c47e3e53306527b84e927c4f
Florent <eliteman.fm@gmail.com>
2020-09-15 10:39:45 +0200

    add members listeners + invitations counter + setup cog extensions

---
+++ core/event/invitation-manager.py
@@ -0,0 +1,53 @@
+from discord.ext import commands
+from main import bot
+from main import invites
+
+members_invites_counter = {}
+
+
+class Invitation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def find_invite_by_code(self, invite_list, code):
+        # Simply looping through each invite in an
+        # invite list which we will get using guild.invites()
+
+        for inv in invite_list:
+
+            # Check if the invite code in this element
+            # of the list is the one we're looking for
+
+            if inv.code == code:
+                # If it is, we return it.
+
+                return inv
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.get_channel(719257870822277174)
+        member_name = member.display_name
+        print("Welcome ", member_name, " !")
+        invites_before_join = invites[member.guild.id]
+        invites_after_join = await member.guild.invites()
+        members_counter = member.guild.member_count
+        for invite in invites_before_join:
+            if invite.uses < self.find_invite_by_code(invites_after_join, invite.code).uses:
+                inviter_name = invite.inviter.name
+                members_invites_counter[inviter_name] = 1 if inviter_name not in members_invites_counter else members_invites_counter.get(inviter_name) + 1
+                await channel.send("Hello " + member_name + f" (Members: {members_counter}) (Invited by: {invite.inviter.name}) (Invitations: {members_invites_counter.get(inviter_name)})")
+
+                invites[member.guild.id] = invites_after_join
+                return
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = member.guild.get_channel(719257870822277174)
+        member_name = member.display_name
+        print("Bye! See you next time ", member_name)
+        invites[member.guild.id] = await member.guild.invites()
+        await channel.send("Bye " + member_name + " (Members: " + str(member.guild.member_count) + ")")
+
+
+def setup(bot):
+    bot.add_cog(Invitation(bot))
--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import discord
+from discord.ext import commands
+
+bot = discord.Client()
+
+invites = {}
+
+# this specifies what extensions to load when the bot starts up
+startup_extensions = ["core.event.invitation-manager"]
+
+bot = commands.Bot(command_prefix='?')
 
 
-class Main(discord.Client):
+@bot.event
+async def on_ready():
+    print("Bot Ready!")
 
-    async def on_ready(self):
-        print("Bot Ready!")
+    # Getting all the guilds our bot is in
+    for guild in bot.guilds:
+        # Adding each guild's invites to our dict
+        invites[guild.id] = await guild.invites()
 
 
-client = Main()
-token = ""
-client.run(token)
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+            print('Extensions: ' + extension + ' loaded!')
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+
+    token = "NzQwNTg3MjYyMzY1OTI1NDE3.XyrLog.H3J6BO934a64bzspWypTYV5cFaY"
+    bot.run(token)
