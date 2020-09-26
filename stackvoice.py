13af033b9927d3152f11a73d0134e06d2b266007
kalicrafty <64757823+kalicrafty@users.noreply.github.com>
2020-09-26 16:18:38 +0200

    Update chadd.py and stackvoice.py

--- stackvoice.py
+++ stackvoice.py
@@ -1,20 +1,23 @@
 import discord
-from [bot variable file] import bot
-import [db file]
+from toolbox.botvar import bot
+import toolbox.utils.db as db
+from toolbox.utils.errors import error
+from toolbox.utils.errors import error
 
 @bot.command()
 async def stackvoice(ctx, channelname:str, channeltype:str):
-    if ctx.author.top_role < ctx.me.top_role: await ctx.send( "You are not a server administrator !"); return
+    if ctx.message.author.guild_permissions.manage_messages or ctx.message.author.guild_permissions.administrator:
+        if channeltype.lower() not in ["yes", "y", "no", "n"]: await error(ctx, "unkown privacy type, please read the help command"); return 
+        if channeltype.lower() == "yes": channeltype = "y"
+        if channeltype.lower() == "no": channeltype = "n"
+        channeltype = channeltype.upper()
 
-    if channeltype.lower() not in ["yes", "y", "no", "n"]: await ctx.send("unkown privacy type, please read the help command"); return 
-    if channeltype.lower() == "yes": channeltype = "y"
-    if channeltype.lower() == "no": channeltype = "n"
-    channeltype = channeltype.upper()
+        guild = ctx.message.guild
+        channel = await guild.create_voice_channel(channelname)
+        channelid = channel.id
+        serverid = ctx.message.guild.id
 
-    guild = ctx.message.guild
-    channel = await guild.create_voice_channel(channelname)
-    channelid = channel.id
-    serverid = ctx.message.guild.id
-
-    db.addChannel(serverid, channelid, channeltype)
-    await ctx.send(f":white_check_mark: a stacked chanel named `{channelname}` was sucessfully created !")
+        db.addChannel(serverid, channelid, channeltype)
+        await ctx.send(f":white_check_mark: a stacked chanel named `{channelname}` was sucessfully created !")
+    else:
+        await error(ctx, "ouuuups, you can't do that :/")
